6ccb29791b354001c06fe0a6bdd4797e4f425e98
sjf0115 <1203745031@qq.com>
2015-07-20 11:08:25 +0800

    Create [剑指offer]7.从尾到头打印链表.cpp

---
+++ [剑指offer]7.从尾到头打印链表.cpp
@@ -0,0 +1,50 @@
+/*---------------------------------------
+*   日期：2015-07-20
+*   作者：SJF0115
+*   题目: 7.从尾到头打印链表
+*   结果：AC
+*   网址：http://www.nowcoder.com/books/coding-interviews/d0267f7f55b3412ba93bd35cfa8e8035?rp=1
+*   来源：剑指Offer
+*   博客：
+-----------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr){}
+};
+
+class Solution {
+public:
+    vector<int> printListFromTailToHead(ListNode* head) {
+        vector<int> result;
+        ListNode* p = head;
+        while(p){
+            result.insert(result.begin(),p->val);
+            p = p->next;
+        }//while
+        return result;
+    }
+};
+
+int main(){
+    Solution s;
+    ListNode* root = new ListNode(1);
+    ListNode* node1 = new ListNode(2);
+    ListNode* node2 = new ListNode(3);
+    ListNode* node3 = new ListNode(4);
+    ListNode* node4 = new ListNode(5);
+    root->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    vector<int> result = s.printListFromTailToHead(root);
+    for(int i = 0;i < result.size();++i){
+        cout<<result[i]<<endl;
+    }//for
+    return 0;
+}
